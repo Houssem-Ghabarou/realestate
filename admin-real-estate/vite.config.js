8a9448e4ed493e6ba713ccf1cdcccb6263072475
root <root@vps-692f08ef.vps.ovh.net>
2023-12-26 23:59:36 +0000

    fix auth syntax

--- admin-real-estate/vite.config.js
+++ admin-real-estate/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "classic", // Add this line
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
   server: {
     host: "0.0.0.0",
     port: 5000,
   },
 });
+
