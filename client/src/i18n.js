8907eae66d3b9a6c265ef290d73525111656aac3
Houssem Ghabarou <44008549+Houssem-Ghabarou@users.noreply.github.com>
2024-01-06 21:53:40 +0100

    LANGUAGE FIX

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,18 +2,30 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+
+// Function to get the selected language from storage or use the fallback
+const getStoredLanguage = () => {
+  const storedLanguage = localStorage.getItem("i18nextLng");
+  return storedLanguage || "fr"; // Fallback to French if not found
+};
+
+const selectedLanguage = getStoredLanguage();
+
 i18n
-
   .use(Backend)
-
   .use(LanguageDetector)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
-    fallbackLng: "fr", // Fallback language if the selected one is not available
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    fallbackLng: "fr",
+    detection: {
+      order: ["navigator"],
+      lookupFromPathIndex: 0,
+      checkWhitelist: true,
     },
+    interpolation: {
+      escapeValue: false,
+    },
+    lng: selectedLanguage, // Set the default language to the stored or fallback language
   });
 
 export default i18n;
